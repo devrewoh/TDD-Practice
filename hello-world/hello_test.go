9371dfb275bcd25a867c613ec6bf987a62358ee4
devrewoh <137814665+devrewoh@users.noreply.github.com>
2023-11-28 14:58:34 -0700

    Completed lesson, learned how to build tests and refactorization

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,13 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
+
 	})
-	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Bridgette", "French")
+		want := "Bonjour, Bridgette"
 		assertCorrectMessage(t, got, want)
 	})
 }
